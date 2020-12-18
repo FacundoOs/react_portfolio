21e5952ea6a71b8bf198fe0f0bcb08498b3c7993
FacundoOs <osores.facundo@gmail.com>
2020-12-18 16:56:15 +0100

    modifies projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,51 +1,51 @@
 export const projectData = [
   {
     id: 1,
+    name: 'Matchup mobile app',
+    image:
+      'https://user-images.githubusercontent.com/68663401/102624186-f8570980-4143-11eb-8032-ee26bcfb4daf.png',
+    description:
+      'Final project at Craft Academy. App where development jobs can be offered and developers can apply ',
+    link: '',
+    github: 'https://github.com/FacundoOs/develUp_mobile',
+  },
+  {
+    id: 2,
+    name: 'Digital newspaper',
+    image:
+      'https://d33wubrfki0l68.cloudfront.net/5fdccf4020225f0008d776bd/screenshot.png',
+    description:
+      'This project includes 3 front-end (client, administrator and mobile) made in React and React native. The back-end was build with Ruby on Rails',
+    link: 'https://goodmorningnews.netlify.app/',
+    github: 'https://github.com/FacundoOs/client_user_good_morning_news',
+  },
+  {
+    id: 3,
     name: 'My First Website',
     image:
-      'https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      'https://d33wubrfki0l68.cloudfront.net/5fc770b58cb0056e0c27a0c7/screenshot.png',
     description: 'This was my first project. A podcast with pure HTML and CSS',
     link: 'https://podcastfacundo.netlify.app',
     github: '',
   },
   {
-    id: 2,
-    name: 'Matchup mobile app',
-    image:
-      'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Final project at Craft Academy. App where development jobs can be offered and developers can apply ',
-    link: '',
-    github: 'https://github.com/CraftAcademy/develUp_mobile',
-  },
-  {
-    id: 3,
-    name: 'Digital newspaper',
-    image:
-      'https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    description:
-      'This project includes 3 front-end, for client, administrator and mobile and a back-end that unites them',
-    link: 'https://goodmorningnews.netlify.app/',
-    github: 'https://github.com/CraftAcademy/client_user_good_morning_news',
-  },
-  {
     id: 4,
-    name: 'Funny Jokes',
-    image:
-      'https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    description: 'Using external Api to display random jokes',
-    link: 'https://johans-jokes.netlify.app/',
-    github: 'https://github.com/CraftAcademy/funny_jokes_client_team_2',
-  },
-  {
-    id: 5,
     name: 'Cooper test',
     image:
-      'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      'https://d33wubrfki0l68.cloudfront.net/5f6545741d34ea00075747aa/screenshot.png',
     description:
       'First project joining front-end and back-end. React and Ruby On rails',
     link: 'https://fo-cooper-test.netlify.app/',
     github: 'https://github.com/FacundoOs/cooper_client_react',
+  },
+  {
+    id: 5,
+    name: 'Funny Jokes',
+    image:
+      'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description: 'Using external Api to display random jokes',
+    link: 'https://johans-jokes.netlify.app/',
+    github: 'https://github.com/FacundoOs/funny_jokes_client_team_2',
   },
   {
     id: 6,
@@ -61,9 +61,8 @@ export const projectData = [
     id: 7,
     name: 'Portfolio',
     image:
-      'https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    description:
-      "I like to design for the mobile platform. The challenges to build UI's for smartphones is challenging but extremely rewarding.",
+      'https://d33wubrfki0l68.cloudfront.net/5fc0cfcb696e2500074a76b1/screenshot.png',
+    description: 'Completely built in React with Webpack and Babel',
     link: 'https://facundoosoresportfolio.netlify.app/',
     github: 'https://github.com/FacundoOs/react_portfolio',
   },
