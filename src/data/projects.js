0c670e050e3d5b9aea9eeb62c98577abb3deb989
FacundoOs <osores.facundo@gmail.com>
2021-01-05 10:22:22 +0100

    Add new project to the projects list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,26 @@
 export const projectData = [
   {
     id: 1,
+    name: 'Amazon Clone',
+    image:
+      'https://user-images.githubusercontent.com/68663401/103627438-e942ca80-4f3d-11eb-8c62-13aae8971e92.png',
+    description:
+      'Amazon clone with firebase authentication and payment with Stripe',
+    link: 'https://clone-1b668.web.app/',
+    github: 'https://github.com/FacundoOs/amazon-clone',
+  },
+  {
+    id: 2,
     name: 'Matchup mobile app',
     image:
-      'https://user-images.githubusercontent.com/68663401/102624186-f8570980-4143-11eb-8032-ee26bcfb4daf.png',
+      'https://user-images.githubusercontent.com/68663401/103628536-79354400-4f3f-11eb-9231-67ba001768be.png',
     description:
       'Final project at Craft Academy. App where development jobs can be offered and developers can apply ',
     link: '',
     github: 'https://github.com/FacundoOs/develUp_mobile',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Digital newspaper',
     image:
       'https://d33wubrfki0l68.cloudfront.net/5fdccf4020225f0008d776bd/screenshot.png',
@@ -20,16 +30,17 @@ export const projectData = [
     github: 'https://github.com/FacundoOs/client_user_good_morning_news',
   },
   {
-    id: 3,
+    id: 4,
     name: 'My First Website',
     image:
       'https://d33wubrfki0l68.cloudfront.net/5fc770b58cb0056e0c27a0c7/screenshot.png',
-    description: 'This was my first project. A podcast with pure HTML and CSS',
+    description:
+      'This was my first project studying in Acamica. A podcast with pure HTML and CSS',
     link: 'https://podcastfacundo.netlify.app',
     github: '',
   },
   {
-    id: 4,
+    id: 5,
     name: 'Cooper test',
     image:
       'https://d33wubrfki0l68.cloudfront.net/5f6545741d34ea00075747aa/screenshot.png',
@@ -37,15 +48,6 @@ export const projectData = [
       'First project joining front-end and back-end. React and Ruby On rails',
     link: 'https://fo-cooper-test.netlify.app/',
     github: 'https://github.com/FacundoOs/cooper_client_react',
-  },
-  {
-    id: 5,
-    name: 'Funny Jokes',
-    image:
-      'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    description: 'Using external Api to display random jokes',
-    link: 'https://johans-jokes.netlify.app/',
-    github: 'https://github.com/FacundoOs/funny_jokes_client_team_2',
   },
   {
     id: 6,
